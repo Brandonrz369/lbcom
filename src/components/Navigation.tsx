19647801e82508c31faedec8af37a9b722cf05d1
Brandonrz369 <brandonruizmarketing@gmail.com>
2025-10-18 14:15:54 -0700

    Fix Navigation component: correct import and className backdrop-blur

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import UtilityBar from "./UtilityBar";
+UtilityBar from "./UtilityBar";
 import MainNav from "./MainNav";
 
 export default function Navigation() {
   return (
-    <header id="site-header" className="sticky top-0 z-50 bg-white/80 backdrpop-blur">
+    <header id="site-header" className="sticky top-0 z-50 bg-white/80 backdrop-blur">
       <UtilityBar />
       <MainNav />
     </header>
